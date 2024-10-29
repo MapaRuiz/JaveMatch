ab071d3fdc2f464863bf3db8cf9f2938bab47abf
Mapa Ruiz <crackmaps.mini@gmail.com>
2024-10-29 23:33:49 +0000

    LikeUsersError

--- javematch/src/main/java/gajudama/javematch/model/UserLike.java
+++ javematch/src/main/java/gajudama/javematch/model/UserLike.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -18,9 +19,11 @@ public class UserLike {
     private Long UserLike_id;
 
     @ManyToOne
+    @JoinColumn(name = "usuario_like_id")
     private Usuario usuarioLike;  // User who gives the like
 
     @ManyToOne
+    @JoinColumn(name = "liked_usuario_id")
     private Usuario likedUsuario; // User who receives the like
 
     @Temporal(TemporalType.TIMESTAMP)
