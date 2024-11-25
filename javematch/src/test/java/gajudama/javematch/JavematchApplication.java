a7d24da6502558cfe54cd522dfa098ae4fdbbf4a
Daniel Castro <castro-df@javeriana.edu.co>
2024-11-25 03:32:06 +0000

    Prueba

--- javematch/src/test/java/gajudama/javematch/JavematchApplication.java
+++ javematch/src/test/java/gajudama/javematch/JavematchApplication.java
@@ -1,18 +1,12 @@
 package gajudama.javematch;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javafx.application.Application;
 
 @SpringBootApplication
 public class JavematchApplication {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Application.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", System.getenv("PORT")));
-        app.run(args);
+        SpringApplication.run(JavematchApplication.class, args);
     }
 }
